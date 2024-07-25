2439ef5cd2d1271aa5403cc5ac5ccebf21c26ca2
AlexeyLavrishin <alexeylavrishin@gmail.com>
2024-07-25 14:43:29 +0300

    ref: create uploadComponent

--- src/shared/lib/instance.ts
+++ src/shared/lib/instance.ts
@@ -4,7 +4,7 @@ export  const instance = axios.create({
     baseURL: "https://social-network.samuraijs.com/api/1.0",
     withCredentials: true,
     headers: {
-        'API-KEY': '901196af-03b1-43ed-ba81-7c0d3e'
+        'API-KEY': '901196af-03b1-43ed-ba81-7c0d3e213179'
     }
 })
 
